d9f05e7aa4140be991f7859af81f0860355f9e5b
Aksis21 <james22@sheridancollege.ca>
2024-02-22 14:21:49 -0500

    Task 1b complete. Player script set up and functioning as a prefab.

---
+++ Assets/Week 7/Scripts/Soccer Player.cs
@@ -0,0 +1,41 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerPlayer : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public Color selectedCol;
+    public Color unselectedCol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        selected(true);
+    }
+
+    //You gave me fred. You did this.
+    public void selected(bool fred)
+    {
+        if (fred)
+        {
+            sr.color = selectedCol;
+        }
+        else
+        {
+            sr.color = unselectedCol;
+        }
+    }
+}
